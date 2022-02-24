0e47622db1174ffd17038d865666d03b7eee2265
Julio Cesar Bueno <buenojc@outlook.com>
2022-02-24 20:10:34 -0300

    Setup do next e primeiros componentes

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link"
+
+export default function Page404(){
+    return(
+        <div>
+            <h1>Parece que você se perdeu :O</h1>
+            <Link href='/'>Ir para a Home</Link>
+        </div> 
+     )
+}
