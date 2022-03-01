fbac0fb44ed1138803b81b419ff6c34be0358595
Julio Cesar Bueno <buenojc@outlook.com>
2022-03-01 19:37:28 -0300

    Adiciona estilo da página FAQ

--- pages/faq.js
+++ pages/faq.js
@@ -1,46 +1,22 @@
-import Head from "../src/components/Head";
-import Link from "../src/components/Link";
+import FaqScreen from '../src/screens/FaqScreen';
 
-export async function getServerSideProps() {
+export default FaqScreen; 
 
-    // O getServerSideProps é executado à cada carregamento da página, já o getStaticProps só é rodado no build do site.
-    // Em modo de desenvolvimento os dois são executados à cada carregamento.
-
-    const API_URL_API = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
-
-    const faq = await fetch(API_URL_API)
-                .then(data => data.json())
-                .then(faq => {return faq});
+export async function getStaticProps() {
+    
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    const faq = await fetch(FAQ_API_URL)
+        .then((respostaDoServidor) => {
+            return respostaDoServidor.json();
+        })
+        .then((resposta) => {
+            return resposta;
+        });
 
     return {
       props: {
-         faq
-      }, // will be passed to the page component as props
-    }
-  }
-
-export default function FaqPage({faq}){
-
-    return(
-        <div>
-            <Head>
-                <title>FAQ - Alura Cursos Campanha</title>
-            </Head>
-            
-            <h1>Alura Cursos - Home</h1>
-            <Link href='/'>Ir para a Home</Link>
-
-            <ul>
-                {faq.map(({question, answer}) => {
-                    return(
-                        <li key={question}>
-                            <h2>{question}</h2>
-                            <p>{answer}</p>
-                        </li>
-                    )
-                })}
-            </ul>
-
-        </div> 
-     )
-}
+          qualquercoisa: 'que eu passar aqui',
+          faq,
+      },
+    };
+}  
